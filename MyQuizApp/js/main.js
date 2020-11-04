c55b29ef0fd93cd44814f2062852c69511e7a4af
Koki Akamatsu <list6311@gmail.com>
2020-11-04 13:19:13 +0900

    finish quiz

--- MyQuizApp/js/main.js
+++ MyQuizApp/js/main.js
@@ -4,13 +4,17 @@
     const question = document.getElementById('question');
     const choices = document.getElementById('choices');
     const btn = document.getElementById('btn');
+    const result = document.getElementById('result');
+    const scoreLabel = document.querySelector('#result > p') 
 
-    const quizSet = [
+    const quizSet = shuffle([
         {q: 'What is A?', c:['A0', 'A1', 'A2']},
         {q: 'What is B?', c:['B0', 'B1', 'B2']},
         {q: 'What is C?', c:['C0', 'C1', 'C2']},
-    ];
+    ]);
     let currentNum = 0;
+    let isAnswerd;
+    let score = 0;
     
     function shuffle(arr) {
         for (let i = arr.length -1; i > 0; i--) {
@@ -21,16 +25,30 @@
     }
 
     function checkAnswer(li){
-        if (li.textContent === quizSet[currentNum].c[0]) {
-            console.log('correct');
-        } else {
-            console.log('wrong');
+        // if (isAnswerd === true){
+        if (isAnswerd){
+            return;
         }
+        isAnswerd = true;
+
+        if (li.textContent === quizSet[currentNum].c[0]) {
+            li.classList.add('correct');
+            score++;
+        } else {
+            li.classList.add('wrong');
+        }
+
+        btn.classList.remove('disabled');
     }
     
 
-    function setQuiz(){
+    function setQuiz() {
+        isAnswerd = false;
         question.textContent = quizSet[currentNum].q;
+
+        while(choices.firstChild) {
+            choices.removeChild(choices.firstChild)
+        }
 
         const shuffledChoices = shuffle([...quizSet[currentNum].c]);
         // console.log(quizSet[currentNum].c);
@@ -42,8 +60,28 @@
             })
             choices.appendChild(li);
         });
+
+        if (currentNum === quizSet.length -1) {
+            btn.textContent = 'Show Score';
+        }
     }
 
     setQuiz(); 
+
+    btn.addEventListener('click',() => {
+        if (btn.classList.contains('disabled')) {
+            return;
+        }
+        btn.classList.add('disabled');
+
+        if (currentNum === quizSet.length - 1) {
+            // console.log(`Score: ${score} / ${quizSet.length}`);
+            scoreLabel.textContent = `Score: ${score} / ${quizSet.length}`;
+            result.classList.remove('hidden');
+        } else {
+            currentNum++;
+            setQuiz();
+        }
+    });
 
 }
