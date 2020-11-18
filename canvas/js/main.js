cdbfc70c310122bf232bc70b2ee3749d0d962719
Koki Akamatsu <list6311@gmail.com>
2020-11-18 19:53:09 +0900

    add content

---
+++ canvas/js/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+{
+    function drow() {
+        const canvas = document.querySelector('canvas');
+        if (typeof canvas.getContext === 'undefined'){
+            return;
+        }
+        const ctx = canvas.getContext('2d');
+        // ctx.fillRect(x, y, width, height)
+        ctx.fillStyle = 'pink';
+        ctx.fillRect(50, 50, 50, 50);
+        ctx.strokeStyle = '#f00'
+        ctx.lineWidth = 8;
+        // ctx.lineJoin = 'round'
+        ctx.lineJoin = 'bevel'
+        ctx.strokeRect(50, 50, 50, 50);
+    }
+    drow();
+}
